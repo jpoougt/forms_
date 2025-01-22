462fc070309963ff7f4684099c7b4702ee791974
jpoougt <83376870+jpoougt@users.noreply.github.com>
2025-01-22 15:57:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function loadCountries() {
       reader.onload = function (e) {
         const data = new Uint8Array(e.target.result);
         const workbook = XLSX.read(data, { type: 'array' });
+        
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
         const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
